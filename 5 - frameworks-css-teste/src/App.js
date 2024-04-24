d8fb2df5d3ff786e018b4fb8bda2f0d0503192f5
Micilini <sdanz@hotmail.com>
2024-04-24 18:37:46 -0300

    Lição 5

---
+++ 5 - frameworks-css-teste/src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Olá, mundo!</h1>
+      <p className="mt-2">Este é um exemplo de como usar Tailwind CSS com ReactJS.</p>
+    </div>
+  );
+};
+
+export default App;
